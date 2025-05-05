8d1f194c33a1f30ae2c6a1378173556b2e49e210
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-06 04:01:03 +0800

    Added some code

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -1,10 +1,30 @@
 const bookLibrary = []; // this will store all the books
 
 // create the book construction
-function Book() {
+function Book(title, author, pages, readStatus = Boolean) {
     if (!new.target) { throw Error("You should use NEW in creating Book object"); }
 
-    
+    this.title = title;
+    this.author = author;
+    this.pages = typeof pages === 'number' ? pages : 0;
+    this.readStatus = typeof readStatus === 'boolean' ? readStatus : false;    
 }
 
-const newBook = new Book();
+Book.prototype.saveBook = function () {
+    bookLibrary.push(this);
+}
+
+let getBook = function () {
+    for (const key in bookLibrary) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {bookLibrary element = object[key];
+ bookLibrary
+        }
+    }
+}
+
+const newBook = new Book("Sample Title", "Aldin Moreno", 250 , true);
+const book2 = new Book("Test Sample 2", "Zeke Moreno", 100 , false);
+newBook.saveBook();
+book2.saveBook();
+console.log(getBook());
+//console.log(bookLibrary);
