14a79361971f6de988e40319eaef58c5f7817834
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-06 01:32:24 +0800

    fix file structure

---
+++ js/booklibrary.js
@@ -0,0 +1,10 @@
+const bookLibrary = []; // this will store all the books
+
+// create the book construction
+function Book() {
+    if (!new.target) { throw Error("You should use NEW in creating Book object"); }
+
+    
+}
+
+const newBook = new Book();
