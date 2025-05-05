2ddaa97a14c29b21657904b28609de4253aa4c3b
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-06 05:51:16 +0800

    Wrote a function that store the books in the array

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -3,28 +3,33 @@ const bookLibrary = []; // this will store all the books
 // create the book construction
 function Book(title, author, pages, readStatus = Boolean) {
     if (!new.target) { throw Error("You should use NEW in creating Book object"); }
+    const bookid = '';
 
+    this.bookid = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = typeof pages === 'number' ? pages : 0;
-    this.readStatus = typeof readStatus === 'boolean' ? readStatus : false;    
-}
+    this.readStatus = typeof readStatus === 'boolean' ? readStatus : false;
 
-Book.prototype.saveBook = function () {
-    bookLibrary.push(this);
-}
-
-let getBook = function () {
-    for (const key in bookLibrary) {
-        if (Object.prototype.hasOwnProperty.call(object, key)) {bookLibrary element = object[key];
- bookLibrary
-        }
+    this.getBookData = function () {
+        return `${this.bookid} , ${this.title}, ${this.author}, ${this.pages}, ${this.readStatus}`;
     }
 }
 
-const newBook = new Book("Sample Title", "Aldin Moreno", 250 , true);
-const book2 = new Book("Test Sample 2", "Zeke Moreno", 100 , false);
-newBook.saveBook();
-book2.saveBook();
-console.log(getBook());
-//console.log(bookLibrary);
+function addBookToLibrary(obj) {
+    const [bookid, title, author, pages, readStatus] = obj.split(',');
+    bookLibrary.push({
+        bookid: bookid,
+        title: title,
+        author: author,
+        pages: parseInt(pages),
+        readStatus: readStatus
+    });
+}
+
+const newBook = new Book("Sample Title", "Aldin Moreno", 250, true);
+const book2 = new Book("Test Sample 2", "Zeke Moreno", 100, false);
+
+addBookToLibrary(newBook.getBookData());
+addBookToLibrary(book2.getBookData());
+console.log(bookLibrary);
