f17ba39e0a1d34086f2ee39df4582c5acf4cbe7f
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-07 06:30:39 +0800

    Started with the jscode to add book entry

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -1,5 +1,21 @@
 const bookLibrary = []; // this will store all the books
 
+const saveBookBtn = document.querySelector("#add-book-button");
+
+saveBookBtn.addEventListener('click', createBookEntry);
+
+function createBookEntry() {
+
+    const bookTitle = document.querySelector('#book-title').value;
+    const bookAuthor = document.querySelector('#book-author').value;
+    const bookYear = document.querySelector('#book-year').value;
+    let isReadStatus = document.querySelector('#book-read');
+
+    isReadStatus = (isReadStatus.checked) ? true : false;
+
+    const newBookEntry = new Book(bookTitle, bookAuthor,);
+}
+
 // create the book construction
 function Book(title, author, pages, readStatus = Boolean) {
     if (!new.target) { throw Error("You should use NEW in creating Book object"); }
