1c68284a7c81564013ad31e07cdd093f1d5f3fce
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-07 22:16:37 +0800

    Added css and js files for the book entry display. Started working with the book display on the page

---
+++ js/displayBook.js
@@ -0,0 +1,6 @@
+const mainContentContainer = document.querySelector('.main-container');
+const bookDivContainer = document.createElement('div');
+const paraContent = document.createElement('p');
+
+mainContentContainer.appendChild(bookDivContainer);
+bookDivContainer.appendChild(paraContent);
