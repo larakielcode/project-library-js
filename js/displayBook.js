aaf8a4f50f1f253cf0e9178dc875ed22241b9625
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-07 22:54:46 +0800

    Added script to display books with JS

--- js/displayBook.js
+++ js/displayBook.js
@@ -1,6 +1,18 @@
-const mainContentContainer = document.querySelector('.main-container');
+const mainContainer = document.querySelector('.main-container');
 const bookDivContainer = document.createElement('div');
-const paraContent = document.createElement('p');
+const bookTitlePara = document.createElement('p');
+const bookYearPara = document.createElement('p');
+const bookAuthorPara = document.createElement('p');
 
-mainContentContainer.appendChild(bookDivContainer);
-bookDivContainer.appendChild(paraContent);
+// Added classes to the div generated
+bookDivContainer.classList.add('book-entry-container');
+
+// Append the div to the main container
+mainContainer.appendChild(bookDivContainer);
+bookDivContainer.appendChild(bookTitlePara);
+bookDivContainer.appendChild(bookYearPara);
+bookDivContainer.appendChild(bookAuthorPara);
+
+bookTitlePara.textContent = 'Book Title';
+bookYearPara.textContent = "Published: 2025";
+bookAuthorPara.textContent = "- Aldin Moreno";
