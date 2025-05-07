ddcba6755ab379e0bcda4c201e6ba161082fa81c
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-08 00:13:32 +0800

    Added the script to initialize books display on the project

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -5,94 +5,127 @@ const bookTitle = document.querySelector("#book-title");
 const bookAuthor = document.querySelector("#book-author");
 const bookPages = document.querySelector("#book-pages");
 
+const mainContainer = document.querySelector(".main-container");
+
 saveBookBtn.addEventListener("click", createBookEntry);
 
+displayBook();
+
 function createBookEntry() {
-    let isReadStatus = document.querySelector("#book-read");
+  let isReadStatus = document.querySelector("#book-read");
 
-    /* Start form validation */
-    let validate = formValidation();
+  /* Start form validation */
+  let validate = formValidation();
 
-    if (validate === false) {
-        errorDisplay.style.display = "block";
-    } else {
+  if (validate === false) {
+    errorDisplay.style.display = "block";
+  } else {
+    isReadStatus = isReadStatus.checked ? true : false;
+    // Initialize book
+    const newBookEntry = new Book(
+      bookTitle.value,
+      bookAuthor.value,
+      parseInt(bookPages.value),
+      isReadStatus
+    );
 
-        isReadStatus = isReadStatus.checked ? true : false;
-        // Initialize book
-        const newBookEntry = new Book(
-            bookTitle.value,
-            bookAuthor.value,
-            parseInt(bookPages.value),
-            isReadStatus
-        );
-
-        newBookEntry.addBookToLibrary(); // create the book and save to the array
-        /* console.clear(); */
-        console.table(bookLibrary);
-        closeModal();
-    }
+    newBookEntry.addBookToLibrary(); // create the book and save to the array
+    /* console.clear(); */
+    //console.table(bookLibrary);
+    closeModal();
+  }
 }
 
 // create the book construction
 function Book(title, author, pages, readStatus) {
-    if (!new.target) {
-        throw Error("You should use NEW in creating Book object");
-    }
-    const bookid = "";
+  if (!new.target) {
+    throw Error("You should use NEW in creating Book object");
+  }
+  const bookid = "";
 
-    this.bookid = crypto.randomUUID(); // generate a unique id for the bookx
-    this.title = title;
-    this.author = author;
-    this.pages = typeof pages === "number" ? pages : 0;
-    this.readStatus = typeof readStatus === "boolean" ? readStatus : false;
+  this.bookid = crypto.randomUUID(); // generate a unique id for the bookx
+  this.title = title;
+  this.author = author;
+  this.pages = typeof pages === "number" ? pages : 0;
+  this.readStatus = typeof readStatus === "boolean" ? readStatus : false;
 }
 
 Book.prototype.addBookToLibrary = function () {
-    bookLibrary.push({
-        bookid: this.bookid,
-        title: this.title,
-        author: this.author,
-        pages: parseInt(this.pages),
-        readStatus: this.readStatus,
-    });
+  bookLibrary.push({
+    bookid: this.bookid,
+    title: this.title,
+    author: this.author,
+    pages: parseInt(this.pages),
+    readStatus: this.readStatus,
+  });
+  displayBook();
 };
 
 function formValidation() {
-    let index;
+  let index;
 
-    if (bookTitle.value == "") {
-        if (!emptyFields.includes(bookTitle)) {
-            emptyFields.push(bookTitle);
-        }
-    } else {
-        index = emptyFields.indexOf(bookTitle);
-        if (index > -1) {
-            emptyFields.splice(index, 1);
-        }
+  if (bookTitle.value == "") {
+    if (!emptyFields.includes(bookTitle)) {
+      emptyFields.push(bookTitle);
     }
-    if (bookAuthor.value == "") {
-        if (!emptyFields.includes(bookAuthor)) {
-            emptyFields.push(bookAuthor);
-        }
-    } else {
-        index = emptyFields.indexOf(bookAuthor);
-        if (index > -1) {
-            emptyFields.splice(index, 1);
-        }
+  } else {
+    index = emptyFields.indexOf(bookTitle);
+    if (index > -1) {
+      emptyFields.splice(index, 1);
     }
-    if (bookPages.value == "") {
-        if (!emptyFields.includes(bookPages)) {
-            emptyFields.push(bookPages);
-        }
-    } else {
-        index = emptyFields.indexOf(bookPages);
-        if (index > -1) {
-            emptyFields.splice(index, 1);
-        }
+  }
+  if (bookAuthor.value == "") {
+    if (!emptyFields.includes(bookAuthor)) {
+      emptyFields.push(bookAuthor);
     }
-    if (emptyFields.length > 0) {
-        return false;
-    } else {
-        return true;
+  } else {
+    index = emptyFields.indexOf(bookAuthor);
+    if (index > -1) {
+      emptyFields.splice(index, 1);
     }
+  }
+  if (bookPages.value == "") {
+    if (!emptyFields.includes(bookPages)) {
+      emptyFields.push(bookPages);
+    }
+  } else {
+    index = emptyFields.indexOf(bookPages);
+    if (index > -1) {
+      emptyFields.splice(index, 1);
+    }
+  }
+  if (emptyFields.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function displayBook() {
+
+  const bookDivContainer = document.createElement("div");
+  const bookTitlePara = document.createElement("p");
+  const bookPagesPara = document.createElement("p");
+  const bookAuthorPara = document.createElement("p");
+
+  if (bookLibrary.length > 0) {
+    mainContainer.appendChild(bookDivContainer);
+    bookDivContainer.classList.add('book-entry-container');
+    bookDivContainer.appendChild(bookTitlePara);
+    bookDivContainer.appendChild(bookPagesPara);
+    bookDivContainer.appendChild(bookAuthorPara);
+    for (const key in bookLibrary) {
+      bookTitlePara.textContent = bookLibrary[key].title;
+      bookPagesPara.textContent = `${bookLibrary[key].pages} pages`;
+      bookAuthorPara.textContent = bookLibrary[key].author;
+      //bookTitlePara.textContent = bookLibrary[key].title;
+
+      //console.log(bookLibrary[key].title);
+      //console.log(bookLibrary[key].pages);
+      //console.log(bookLibrary[key].author);
+      //console.log(bookLibrary[key].readStatus);
+    }
+  } else {
+    console.log("No data yet");
+  }
 }
