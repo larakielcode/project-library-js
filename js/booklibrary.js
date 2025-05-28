f3f93dc4f001293283148a155efb845442c71815
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-29 01:44:02 +0800

    working book class + function to display the instantiated books

--- js/booklibrary.js
+++ js/booklibrary.js
@@ -10,17 +10,51 @@ class Books {
     this.hasRead = hasRead;
   }
 
-  getBooks() {
+  addBookToShelf() {
+    bookShelf.push(this);
+    displayBooks(this);
+  }
+
+  getBookInfo() {
     return `bookId:${this.bookId},title:${this.title},author:${this.author},pages:${this.pages},hasRead:${this.hasRead}`;
   }
 
-  saveBooks() {
-    const pairs = this.getBooks().split(',');
-    const results = [];
-    for (const pair of pairs) {
-      const [key, value] = pair.split(':');
-      results.push({ [key]: value });
-    }
-    bookShelf.push(results);
+}
+
+function displayBooks() {
+  const mainContainer = document.querySelector('.main-container');
+  const bookDivContainer = document.createElement('div');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const bookPages = document.createElement('p');
+  const bookHasRead = document.createElement('p');
+  const readStatusBtn = document.createElement('button');
+
+  mainContainer.appendChild(bookDivContainer);
+  bookDivContainer.classList.add('book-entry-container');
+  bookDivContainer.appendChild(bookTitle);
+  bookDivContainer.appendChild(bookPages);
+  bookDivContainer.appendChild(bookAuthor);
+  bookDivContainer.appendChild(bookHasRead);
+  bookHasRead.appendChild(readStatusBtn);
+
+  for (const key in bookShelf) {
+    bookTitle.textContent = bookShelf[key]['title'];
+    bookAuthor.textContent = bookShelf[key]['author'];
+    bookPages.textContent = bookShelf[key]['pages'];
+    let classbtn = (bookShelf[key]['hasRead'] == true) ? 'readbtn' : 'unreadbtn';
+    readStatusBtn.setAttribute('class', classbtn);
+    (classbtn == 'readbtn') ? readStatusBtn.textContent = 'read' : readStatusBtn.textContent = 'unread';
   }
 }
+
+const book = new Books('test books', 'aldin moreno', '333', false);
+const book1 = new Books('dummies book', 'zeke moreno', '143', true);
+const book2 = new Books('ambot book', 'lotlot moreno', '22', false);
+book.addBookToShelf();
+book1.addBookToShelf();
+book2.addBookToShelf();
+console.log("\n--- Current Books on Shelf ---");
+bookShelf.forEach((book, index) => {
+  console.log(`${index + 1}. ${book.getBookInfo()}`);
+});
